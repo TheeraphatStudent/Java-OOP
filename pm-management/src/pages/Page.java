e372b478f8247f23fb0de8bca7de0ffdba1b6db6
TheeraphatCH <theeraphatchueanokkhum@gmail.com>
2024-07-18 09:03:42 +0700

    feat: setup main page layout

--- pm-management/src/pages/Page.java
+++ pm-management/src/pages/Page.java
@@ -1,22 +1,58 @@
 package pages;
 
-import javax.swing.JFrame;
-import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import components.Dashboard;
+import components.Statistic;
+import components.Footer;
 
 public class Page {
     JFrame frame = new JFrame("PM2.5 Reporter");
+    JPanel panel = new JPanel(new GridLayout(1, 2, 20, 20));
+
+    Color primary_color = new Color(248, 237, 227, 70).darker();
+
+    Dashboard dashboard = new Dashboard();
+    Footer footer = new Footer(primary_color);
+    Statistic statistic = new Statistic();
 
     public void showFrame() {
         frame.setSize(500, 500);
-        frame.setLayout(new GridLayout(20, 10, 5, 5));
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gridConst = new GridBagConstraints();
 
-        JButton getFile = new JButton("Seclect File");
+        frame.getContentPane().setBackground(primary_color);
 
-        frame.add(getFile);
+        // Panel Content
+        panel.setBackground(primary_color);
+        panel.add(dashboard.getDashboard());
+        panel.add(statistic.getStatistic());
+
+        // Main Content
+        gridConst.gridx = 0;
+        gridConst.gridy = 0;
+        gridConst.weightx = 1.0;
+        gridConst.weighty = 0.7;
+        gridConst.fill = GridBagConstraints.BOTH;
+        gridConst.insets = new Insets(20, 20, 20, 20);
+        frame.add(panel, gridConst);
+
+        gridConst.gridx = 0;
+        gridConst.gridy = 1;
+        gridConst.weightx = 1.0;
+        gridConst.weighty = 0.3;
+        gridConst.fill = GridBagConstraints.BOTH;
+        gridConst.insets = new Insets(0, 20, 20, 20);
+        frame.add(footer.getFooter(), gridConst);
 
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
-
     }
 }
